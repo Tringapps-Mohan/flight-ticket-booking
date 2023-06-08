1e4ee9f585e4e09d8f6f93105a8df8d7f20c4d80
Tringapps-Mohan <102800135+Tringapps-Mohan@users.noreply.github.com>
2023-06-08 15:59:29 +0530

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,7 +3,7 @@ const { register, login, logout, getAllBookings } = require("../controllers/admi
 const router = express.Router(); // Create an instance of Express router
 
 // Define the routes and associate them with the corresponding controller functions
-router.post("/register", register); // Route for registering a new admin
+router.post("/signup", register); // Route for registering a new admin
 router.post("/login", login); // Route for admin login
 router.post("/logout", logout); // Route for admin logout
 router.get("/bookings", getAllBookings); // Route for getting all bookings made by users
