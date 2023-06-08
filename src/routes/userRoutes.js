27906706221975cbe8c186c239b8c8eb755560d5
Tringapps-Mohan <102800135+Tringapps-Mohan@users.noreply.github.com>
2023-06-08 15:59:06 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,7 @@ const { register, login, logout, myBookings } = require("../controllers/user.js"
 const { verifyUser } = require("../utils/verifyToken.js"); // Import the verifyUser middleware function for token verification
 const router = express.Router(); // Create an instance of Express router
 
-router.post("/register", register); // Route for user registration
+router.post("/signup", register); // Route for user registration
 router.post("/login", login); // Route for user login
 router.post("/logout", logout); // Route for user logout
 router.get("/mybookings", verifyUser, myBookings); // Route for retrieving user's bookings, accessible to authenticated users
