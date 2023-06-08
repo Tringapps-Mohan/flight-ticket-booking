4825740bddc2e1b825461ed327424827c392e41d
Tringapps-Mohan <102800135+Tringapps-Mohan@users.noreply.github.com>
2023-06-08 12:23:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// Import required modules
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
@@ -5,42 +6,54 @@ const userRoute = require("./routes/userRoutes.js");
 const adminRoute = require("./routes/adminRoutes.js");
 const flightRoute = require("./routes/flightRoutes.js");
 const cookieParser = require("cookie-parser");
+
+// Create an instance of the Express application
 const app = express();
+
+// Load environment variables from .env file
 dotenv.config();
 
+// Connect to MongoDB database
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
-        console.log("connected to mongodb");
+        console.log("Connected to MongoDB");
     } catch (error) {
         console.log(error);
     }
 }
 
+// Middleware for parsing cookies
 app.use(cookieParser());
+
+// Middleware for parsing JSON request bodies
 app.use(express.json());
 
+// Define routes for different endpoints
 app.use("/users", userRoute);
-app.use("/admin",adminRoute);
-app.use("/flights",flightRoute);
+app.use("/admin", adminRoute);
+app.use("/flights", flightRoute);
 
-app.use((err,req,res,next)=>{
+// Error handling middleware
+app.use((err, req, res, next) => {
     const errorStatus = err.status || 500;
     const errorMessage = err.message || "Something went wrong";
     return res.status(errorStatus).json({
-        success:false,
-        status:errorStatus,
-        message:errorMessage,
-        stack:err.stack
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
     });
-})
-
-
-mongoose.connection.on("disconnected",()=>{
-    console.log("mongodb disconnected");
 });
 
+// Event listener for MongoDB disconnection
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
+});
+
+// Start the server
 app.listen(1000, () => {
+    // Connect to MongoDB when the server starts
     connect();
     console.log("Server started.");
 });
