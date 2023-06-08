7b2dc4657b5beb02b96261898615472e0cfde9cc
Tringapps-Mohan <102800135+Tringapps-Mohan@users.noreply.github.com>
2023-06-08 12:26:03 +0530

    Update error.js

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,7 +1,9 @@
-const createError = (message,status)=>{
-    const err = new Error();
-    err.status = status;
-    err.message = message;
-    return err;
+// Create and export a function for creating custom errors
+const createError = (message, status) => {
+    const err = new Error(); // Create a new Error object
+    err.status = status; // Set the error status
+    err.message = message; // Set the error message
+    return err; // Return the created error object
 };
-module.exports = createError;
+
+module.exports = createError; // Export the createError function for use in other files
