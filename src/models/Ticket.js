f2bf36d2b77d0a6d2206df56b334eff4ae4f6a73
Tringapps-Mohan <102800135+Tringapps-Mohan@users.noreply.github.com>
2023-06-08 12:54:47 +0530

    Update Ticket.js

--- src/models/Ticket.js
+++ src/models/Ticket.js
@@ -1,31 +1,33 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // Import the Mongoose library
+
+// Define the Ticket schema
 const TicketSchema = new mongoose.Schema({
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
     },
-    flightId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Flight",
-        required:true,
+    flightId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Flight",
+        required: true,
     },
-    seat:{
-        type:Number,
-        required:true
+    seat: {
+        type: Number,
+        required: true
     },
-    passengerName:{
-        type:String,
-        required:true
+    passengerName: {
+        type: String,
+        required: true
     },
-    boardingDate:{
-        type:Date,
-        required:true
+    boardingDate: {
+        type: Date,
+        required: true
     },
-    boardingTime:{
-        type:String,
-        required:true
+    boardingTime: {
+        type: String,
+        required: true
     }
 });
 
-module.exports = mongoose.model("Ticket",TicketSchema);
+module.exports = mongoose.model("Ticket", TicketSchema); // Export the Ticket model
