280185e9727c9d76f3a103ada3c645e19dca55b8
Tringapps-Mohan <102800135+Tringapps-Mohan@users.noreply.github.com>
2023-06-08 13:39:00 +0530

    Update user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,51 +4,78 @@ const createError = require("../utils/error.js");
 const jwt = require("jsonwebtoken");
 
 module.exports = {
-    register: async (req, res, next) => {
-        try {
-            const salt = bcrypt.genSaltSync();
-            const hash = bcrypt.hashSync(req.body.password, salt);
-            const user = new User({
-                ...req.body,
-                password: hash
-            })
-            await user.save();
-            res.status(200).send("New User created successfully.");
-        } catch (err) {
-            next(err);
-        }
-    },
-    login: async (req, res, next) => {
-        try {
-            const user = await User.findOne({
-                username: req.body.username
-            });
-            if (!user)
-                return next(createError(404, "User not found!"));
-            const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+  // Function to register a new user
+  register: async (req, res, next) => {
+    try {
+      // Generate a salt and hash the password
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(req.body.password, salt);
 
-            if (!isPasswordCorrect)
-                return next(createError(400, "Wrong password or username."));
+      // Create a new user with the provided data and hashed password
+      const user = new User({
+        ...req.body,
+        password: hash
+      });
 
-            const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT);
-            const { password, ...others } = user._doc;
-            res.cookie("access_token", token, {
-                httpOnly: true,
-            }).status(200).json({ ...others });
-        } catch (err) {
-            next(err);
-        }
-    },
-    logout: (req, res, next) => {
-        res.clearCookie("token");
-        res.json({ message: "Logged out successfully" });
-    },
-    myBookings: async (req, res, next) => {
-        try {
-            const user = await User.findById(req.user.id);
-            res.status(200).json(user.bookedFlights);
-        } catch (error) {
-            next(error);
-        }
+      // Save the new user to the database
+      await user.save();
+      res.status(200).send("New User created successfully.");
+    } catch (err) {
+      next(err);
     }
-}
+  },
+
+  // Function to handle user login
+  login: async (req, res, next) => {
+    try {
+      // Find the user by their username
+      const user = await User.findOne({
+        username: req.body.username
+      });
+
+      // If user is not found, return an error
+      if (!user) {
+        return next(createError(404, "User not found!"));
+      }
+
+      // Compare the provided password with the hashed password stored in the database
+      const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+
+      // If the passwords don't match, return an error
+      if (!isPasswordCorrect) {
+        return next(createError(400, "Wrong password or username."));
+      }
+
+      // Generate a JSON Web Token (JWT) for the user
+      const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT);
+
+      // Remove the password field from the user object
+      const { password, ...others } = user._doc;
+
+      // Set the access_token cookie with the JWT and send the user object in the response
+      res.cookie("access_token", token, {
+        httpOnly: true
+      }).status(200).json({ ...others });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  // Function to handle user logout
+  logout: (req, res, next) => {
+    // Clear the access_token cookie and send a success message
+    res.clearCookie("token");
+    res.json({ message: "Logged out successfully" });
+  },
+
+  // Function to get the bookings of the authenticated user
+  myBookings: async (req, res, next) => {
+    try {
+      // Find the user by their ID and return their bookedFlights array
+      const user = await User.findById(req.user.id);
+      res.status(200).json(user.bookedFlights);
+    } catch (error) {
+      next(error);
+    }
+  }
+};
